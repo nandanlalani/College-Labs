17d06f2f82ec2fadd4e441c754a841768bd377ee
Nandan Lalani <80481008+nandanlalani@users.noreply.github.com>
2025-07-01 07:55:36 +0530

    Add files via upload

---
+++ lib/lab3/crud_db_32/data_model.dart
@@ -0,0 +1,26 @@
+class UserModel {
+  int? uid;
+  String name;
+  String city;
+  String gender;
+
+  UserModel({this.uid, required this.name, required this.city, required this.gender});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'uid': uid,
+      'name': name,
+      'city': city,
+      'gender': gender,
+    };
+  }
+
+  factory UserModel.fromMap(Map<String, dynamic> map) {
+    return UserModel(
+      uid: map['uid'],
+      name: map['name'],
+      city: map['city'],
+      gender: map['gender'],
+    );
+  }
+}
